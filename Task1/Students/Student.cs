8e90e0fdb6458ea40fc7b89edcd324449c252c22
Pavelsch00 <58011434+NooBit0@users.noreply.github.com>
2020-08-08 17:42:06 +0300

    Interface and class Student is commented.

--- Task1/Students/Student.cs
+++ Task1/Students/Student.cs
@@ -3,14 +3,27 @@ using Task1.Students.Interfaces;
 
 namespace Task1.Students
 {
+    /// <summary>
+    /// Class describes the student.
+    /// </summary>
     public class Student : IStudent, IComparable
     {
         private int assessment;
 
+        /// <summary>
+        /// An empty constructor is needed for serialization.
+        /// </summary>
         public Student()
         {
         }
 
+        /// <summary>
+        /// The constructor initializes the class object.
+        /// </summary>
+        /// <param name="name">Student name.</param>
+        /// <param name="testName">Test name.</param>
+        /// <param name="testDate">Date of the test.</param>
+        /// <param name="assessment">Test assessment.</param>
         public Student(string name, string testName, string testDate, int assessment)
         {
             Name = name;
@@ -19,12 +32,16 @@ namespace Task1.Students
             Assessment = assessment;
         }
 
+        /// <inheritdoc cref="IStudent.Name"/>
         public string Name { get; set; }
 
+        /// <inheritdoc cref="IStudent.TestName"/>
         public string TestName { get; set; }
 
+        /// <inheritdoc cref="IStudent.TestDate"/>
         public string TestDate { get; set; }
 
+        /// <inheritdoc cref="IStudent.Assessment"/>
         public int Assessment
         {
             get => assessment;
@@ -37,12 +54,22 @@ namespace Task1.Students
             }
         }
 
+        /// <summary>
+        /// Method comparing two objects.
+        /// </summary>
+        /// <param name="obj">Comparison object.</param>
+        /// <returns>Returns the result of the comparison</returns>
         public int CompareTo(object obj)
         {
            Student student = (Student)obj;
            return Assessment.CompareTo(student.Assessment);
         }
 
+        /// <summary>
+        /// Method equals two objects.
+        /// </summary>
+        /// <param name="obj">Equals object.</param>
+        /// <returns>Returns the result of the comparison</returns>
         public override bool Equals(object obj)
         {
             if (obj.GetType() != GetType())
@@ -56,8 +83,16 @@ namespace Task1.Students
                    Assessment == student.Assessment;
         }
 
+        /// <summary>
+        /// The method gets the hash code of the object.
+        /// </summary>
+        /// <returns>Returns the hash code of the object.</returns>
         public override int GetHashCode() => HashCode.Combine(Name, TestName, TestDate, Assessment);
 
+        /// <summary>
+        /// The method returns information about the object in string form.
+        /// </summary>
+        /// <returns>Information about the object.</returns>
         public override string ToString() => $"Name: {Name}, Test Name: {TestName}, TestDate: {TestDate}, Assessment: {Assessment}\n";
     }
 }
